23ef8179f819e19d7c77f44db01e8e522f2bfdee
Alfredoeb9 <alfredoeb96@gmail.com>
2023-12-03 20:16:26 -0800

    [query] set up inital query to get all todos

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
-
+const cors = require('cors')
 const app = express();
+const pool = require('./db')
+
+app.use(cors())
+
+// get all todos
+app.get('/todo/:userEmail', async (req, res) => {
+    const { userEmail } = req.params
+    try {
+        const allTodos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
+        res.json(allTodos.rows)
+    } catch(err) {
+        console.log('error', err)
+    }
+})
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
